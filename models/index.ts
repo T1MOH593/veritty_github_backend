5bf41ae3035cb78b468a4f9a81d35063491041ca
Vlad Andrievski <andryieuski@papaya.land>
2023-01-28 19:05:03 +0300

    init

--- models/index.ts
+++ models/index.ts
@@ -7,7 +7,7 @@ import { GoogleSpreadsheet } from "google-spreadsheet"
 import { Sequelize, DataTypes, Op } from 'sequelize';
 
 const app = express();
-const port = 5001;
+const port = process.env.PORT || 5001;
 
 import { BigNumber } from '@moralisweb3/core';
 
